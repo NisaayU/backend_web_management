f4c7b56726d3cf60154d8ad1e623f199928c1502
Nisaayu <ayy515951@gmail.com>
2026-04-04 20:14:21 +0700

    fix: update CORS config

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
@@ -5,13 +7,21 @@ const { readdirSync } = require('fs')
 const { seedDefaultUsers } = require('./controllers/user');
 const app = express()
 
-require('dotenv').config()
 
 const PORT = process.env.PORT
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'https://afcaa306-8731-44cf-9818-91461b831ea0-00-b0ch5nd4x1em.sisko.replit.dev',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // routes
 readdirSync('./routes').map((route) =>
